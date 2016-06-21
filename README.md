8bc0de1792e4f48a8b6ec7d99a53589a07bafc70
arin <adt608@hotmail.com>
2016-06-20 21:14:49 -0500

    Removing old files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ridge
-TBS python game
--- test.sh
+++
@@ -1,7 +0,0 @@
-. activate
-python tests/test_map.py
-python tests/test_unit.py
-python tests/test_model_manipulation.py
-python tests/test_special_attack.py
-python tests/test_tile.py 
-deactivate
