1e7a94d96ed29ad05f24a62587a98f00d88407e0
arin <adt608@hotmail.com>
2016-07-03 21:42:55 -0500

    Updated test script

--- cpp/test.sh
+++ cpp/test.sh
@@ -1,7 +1,7 @@
-./base_unit_unittest
-./base_tile_unittest
-./constants_unittest
-./map_unittest
-./sub_tile_unittest
-./sub_unit_unittest
+./build/base_unit_unittest
+./build/base_tile_unittest
+./build/constants_unittest
+./build/map_unittest
+./build/sub_tile_unittest
+./build/sub_unit_unittest
 
